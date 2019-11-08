a6d2792ad3b03099577b3022d4c102d3fc95c34d
karlaevelize <k.evelize@gmail.com>
2019-11-08 11:37:42 +0100

    Added form validation

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,24 +1,64 @@
+//validation function
+function doesNotPassAllValidations(name, message) {
+    if (!name && !message){
+        alert("Please don't send it empty")
+        return true
+    }
+    if (!name) {
+        alert('Oh, these muggles technologies! You have to fill your name!')
+        return true
+    }
+    if (!message) {
+        alert("And don't forget your message, it's faster than an owl!")
+        return true
+    }
+    if (message.length > 280) {
+        alert("Writing this much, best go to the Daily Prophet. We need no Rita Skeeter here!")
+        return true
+    }
+    return false
+}
+
+//change the first letter to upperCase
+function upperCase(name){
+
+}
+
 //add event handler
-function submitComment (){
-//gather date
+function submitComment () {
+
+//so the page does not refresh
+    event.preventDefault()
+
+//gather data
     const inputField = document.getElementById('name')
     const name = inputField.value
-    const textArea = document.getElementById('msg')
-    const msg = textArea.value
+    const textArea = document.getElementById('message')
+    const message = textArea.value
+
+// check if user input passes validations
+    if(doesNotPassAllValidations(name, message)){
+        return null
+    }
+
 //create the elements you need
     const comment = document.createElement('section')
     const h3 = document.createElement('h3')
     const p = document.createElement('p')
-//adjust the elements we created
+
     h3.innerHTML = `${name} said:`
-    p.innerHTML = msg
+    p.innerHTML = message
+
+//add a class to a new section
     comment.classList.add('comment')
+    
     comment.appendChild(h3)
     comment.appendChild(p)
-    console.log('comment')
+
 // display the elements on the page
     const commentSection = document.getElementById("comments");
     commentSection.appendChild(comment);
+
 
 // reset form values
     inputField.value = null;
